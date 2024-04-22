594427f20169c12f49cffd919ce1a6273bd5f1bb
spooky-th-ghost <colin@wildheartinteractive.com>
2024-04-21 21:27:37 -0400

    Snap to pixels

--- src/camera.rs
+++ src/camera.rs
@@ -2,7 +2,22 @@ use crate::input::{InputBuffer, PlayerAction};
 use crate::player::PlayerData;
 use crate::GameState;
 
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::{
+        camera::RenderTarget,
+        render_resource::{
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+        },
+        view::RenderLayers,
+    },
+    window::WindowResized,
+};
+
+const RES_WIDTH: u32 = 854;
+const RES_HEIGHT: u32 = 480;
+
+const HIGH_RES_LAYER: RenderLayers = RenderLayers::layer(1);
 
 pub struct CameraPlugin;
 
@@ -10,6 +25,7 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::Overworld), spawn_camera)
             .insert_resource(CameraData::default())
+            .insert_resource(Msaa::Off)
             .add_plugins(TraditionalCameraPlugin)
             .register_type::<MainCamera>();
     }
@@ -26,6 +42,7 @@ impl Plugin for TraditionalCameraPlugin {
                 position_camera.after(crate::physics::systems::lateral_movement),
                 rotate_camera,
                 adjust_offset,
+                fit_canvas,
             )
                 .run_if(in_state(GameState::Overworld)),
         );
@@ -76,6 +93,15 @@ pub enum CameraMode {
     Follow,
 }
 
+#[derive(Component)]
+struct Canvas;
+
+#[derive(Component)]
+struct InGameCamera;
+
+#[derive(Component)]
+struct OuterCamera;
+
 impl CameraMode {
     fn shift_up(&self) -> CameraMode {
         match self {
@@ -92,10 +118,48 @@ impl CameraMode {
         }
     }
 }
-fn spawn_camera(mut commands: Commands) {
-    commands
-        .spawn(Camera3dBundle::default())
-        .insert(MainCamera {
+fn spawn_camera(
+    mut commands: Commands,
+    mut images: ResMut<Assets<Image>>,
+    windows: Query<&Window>,
+) {
+    let canvas_size = Extent3d {
+        width: RES_WIDTH,
+        height: RES_HEIGHT,
+        ..default()
+    };
+
+    let mut canvas = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size: canvas_size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    canvas.resize(canvas_size);
+
+    let image_handle = images.add(canvas);
+
+    // The Camera that renders our pixelated view to the canvas
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                order: -1,
+                target: RenderTarget::Image(image_handle.clone()),
+                ..default()
+            },
+            ..default()
+        },
+        MainCamera {
             offset: Vec3::new(0.0, 6.5, 10.0),
             y_offset_max: 9.5,
             y_offset_min: 4.5,
@@ -103,7 +167,53 @@ fn spawn_camera(mut commands: Commands) {
             easing: 4.0,
             camera_mode: CameraMode::Free,
             desired_position: Vec3::ZERO,
-        });
+        },
+        InGameCamera,
+    ));
+
+    // Canvas that the main camera is rendered to
+    commands.spawn((
+        SpriteBundle {
+            texture: image_handle,
+            ..default()
+        },
+        Canvas,
+        HIGH_RES_LAYER,
+    ));
+
+    let window = windows.single();
+
+    commands.spawn((
+        Camera2dBundle {
+            projection: OrthographicProjection {
+                near: -1000.0,
+                scale: calculate_outter_camera_scale(
+                    window.physical_width() as f32,
+                    window.physical_height() as f32,
+                ),
+                ..default()
+            },
+            ..default()
+        },
+        OuterCamera,
+        HIGH_RES_LAYER,
+    ));
+}
+
+fn fit_canvas(
+    mut resize_events: EventReader<WindowResized>,
+    mut projections: Query<&mut OrthographicProjection, With<OuterCamera>>,
+) {
+    for event in resize_events.read() {
+        let mut projection = projections.single_mut();
+        projection.scale = calculate_outter_camera_scale(event.width, event.height);
+    }
+}
+
+fn calculate_outter_camera_scale(width: f32, height: f32) -> f32 {
+    let h_scale = width / RES_WIDTH as f32;
+    let v_scale = height / RES_HEIGHT as f32;
+    0.8 / h_scale.min(v_scale).round()
 }
 
 fn update_camera_desired_position(
