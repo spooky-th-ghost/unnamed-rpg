b3c90e20e329628ace365610dc07efae829ba52e
spooky-th-ghost <colin@wildheartinteractive.com>
2024-05-08 10:50:57 -0400

    rework in progress

--- src/player.rs
+++ src/player.rs
@@ -24,7 +24,7 @@ impl Plugin for PlayerPlugin {
                 (
                     set_player_direction,
                     play_idle_animation,
-                    transition_player_state,
+                    transition_player_state.run_if(on_event::<AnimationTransitionEvent>()),
                     update_player_data,
                     jump,
                     handle_regrab,
