c61ec1a2ba198eb06f257e454fdaee026bde18ea
spooky-th-ghost <colin@wildheartinteractive.com>
2024-05-10 21:58:01 -0400

    now you gotta press a button

--- src/item.rs
+++ src/item.rs
@@ -1,4 +1,4 @@
-use crate::player::Player;
+use crate::{input::InputBuffer, player::Player};
 use bevy::prelude::*;
 use bevy_xpbd_3d::prelude::*;
 
@@ -72,11 +72,11 @@ fn pickup_items(
     // a resource that holds the players inventory
     mut inventory: ResMut<Inventory>,
     // A query that finds the player entity
-    player_query: Query<Entity, With<Player>>,
+    player_query: Query<(Entity, &InputBuffer), With<Player>>,
     // A query that finds all entities with an OverWorldItem Component
     item_query: Query<&OverworldItem>,
 ) {
-    if let Ok(player_entity) = player_query.get_single() {
+    if let Ok((player_entity, intput_buffer)) = player_query.get_single() {
         // 1. Use collisions to find all entities colliding with the player entity
         for collision in collisions.collisions_with_entity(player_entity) {
             // 2. Check those collision pairs to see if one of the entites is the player and the other is
@@ -89,11 +89,14 @@ fn pickup_items(
             };
 
             if let Ok(overworld_item) = item_query.get(item_entity) {
-                // 3. Add the item to the players inventory
-                // add overworld item to inventory
-                inventory.add_to_inventory(overworld_item.id);
-                // 4. Despawn the item
-                commands.entity(item_entity).despawn_recursive();
+                // 3. if the player is pressing the interact button
+                if intput_buffer.just_pressed(crate::input::PlayerAction::Interact) {
+                    // 4. Add the item to the players inventory
+                    // add overworld item to inventory
+                    inventory.add_to_inventory(overworld_item.id);
+                    // 5. Despawn the item
+                    commands.entity(item_entity).despawn_recursive();
+                }
             }
         }
     }
